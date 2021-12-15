2ff45021488edb0e5537e3ccff225854b43092ba
vladloh <54853590+vladloh@users.noreply.github.com>
2021-12-16 01:06:23 +0300

    security

--- vk_parser/config.py
+++ vk_parser/config.py
@@ -1,7 +1,7 @@
 """Important constants."""
 import types
 
-VK_TOKEN = '662f0d39662f0d39662f0d39bf664121a36662f662f0d393bf5264d2dffbae81b0a5917'
+VK_TOKEN = '*******'
 DIR_FRIENDS = 'friends'
 CANT_GET_FRIENDS = types.MappingProxyType({
     30: 'private_profile',
