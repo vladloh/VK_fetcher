fead9535b24ba5f08e1277264412c4086724799a
vladloh <54853590+vladloh@users.noreply.github.com>
2021-12-16 22:07:14 +0300

    bug

--- vk_parser/config.py
+++ vk_parser/config.py
@@ -16,7 +16,7 @@ config = configparser.ConfigParser()
 config.read(TOKEN_PATH)
 
 try:
-    VK_TOKEN = config['VK_API']['VK_TOKEN2']
+    VK_TOKEN = config['VK_API']['VK_TOKEN']
 except Exception:
     sys.stdout.write('Add VK_TOKEN to VK_API section in setup.cfg')
     VK_TOKEN = None
