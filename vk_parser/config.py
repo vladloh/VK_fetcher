9a44a11fdd9bd5748a02a804dca0b7ef8c21fa2e
vladloh <54853590+vladloh@users.noreply.github.com>
2021-12-16 16:57:15 +0300

    sql added

--- vk_parser/config.py
+++ vk_parser/config.py
@@ -1,7 +1,7 @@
 """Important constants."""
 import types
 
-VK_TOKEN = '662f0d39662f0d39662f0d39bf664121a36662f662f0d393bf5264d2dffbae81b0a5917'
+VK_TOKEN = '******'
 DATABASE_PATH = 'friendlists.db'
 DATABASE_NAME = 'friends'
 CANT_GET_FRIENDS_ERROR_CODES = types.MappingProxyType({
